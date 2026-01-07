03af60190454e718c6c2fca95e53d3c7ed4f1459
Avinash-Rohra <avibest1@yahoo.com>
2026-01-07 20:35:45 +0800

    Update config.py

--- exp_results/QNRF-Baseline/config.py
+++ exp_results/QNRF-Baseline/config.py
@@ -9,15 +9,15 @@ cfg = __C
 
 #------------------------------TRAIN------------------------
 __C.SEED = 3035  # random seed,  for reproduction
-__C.DATASET = 'QNRF'  # dataset selection: NWPU, SHHA, SHHB, QNRF, FDST
+__C.DATASET = 'QNRF'  
 
 
-__C.NET = 'VGG16_FPN' #  optional ['HR_Net', 'VGG16_FPN']
+__C.NET = 'Baseline' 
 
-__C.PRE_HR_WEIGHTS = '../PretrainedModels/hrnetv2_w48_imagenet_pretrained.pth'
+__C.PRE_HR_WEIGHTS = '../Models/MSPF.pth'
 
 __C.RESUME = False  # contine training
-__C.RESUME_PATH = './exp/12-28_16-21_QNRF_HR_Net/latest_state.pth'
+__C.RESUME_PATH = './exp_results/latest_state.pth'
 
 __C.GPU_ID = '2,3'  # sigle gpu: [0], [1] ...; multi gpus: [0,1]
 
